a262bf1f1076aeb386090c77d96855599dc963c4
Makcik303123 <adoberoflit@gmail.com>
2024-10-07 14:17:50 +0300

    first commit

---
+++ js/menu.js
@@ -0,0 +1,53 @@
+const burger = document.getElementById("wrapper__icon");
+const openBtn = document.getElementById("secondBtn");
+const closeBtn = document.getElementById("wrapper__close");
+const menu = document.querySelector(".menu__body");
+const header = document.querySelector(".header");
+const fixedHeader = document.querySelector(".fixed__header");
+
+function openBurger() {
+    menu.classList.add("active");
+    document.body.classList.add("lock");
+}
+
+function closeBurger() {
+    menu.classList.remove("active");
+    document.body.classList.remove("lock");
+}
+
+const anchors = document.querySelectorAll(".anchors");
+
+for (let anchor of anchors) {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        menu.classList.remove("active");
+        document.body.classList.remove("lock");
+
+        const blockID = anchor.getAttribute("href").slice(1);
+        let block = "start";
+
+        document.getElementById(blockID).scrollIntoView({
+            behavior: "smooth",
+            block,
+        });
+    });
+}
+
+openBtn.addEventListener("click", openBurger);
+burger.addEventListener("click", openBurger);
+closeBtn.addEventListener("click", closeBurger);
+
+document.addEventListener("click", function (event) {
+    if (!header.contains(event.target) && !fixedHeader.contains(event.target)) {
+        closeBurger();
+    }
+});
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 104) {
+        fixedHeader.classList.add("active"); // Добавляем класс, если скролл больше 80px
+    } else {
+        fixedHeader.classList.remove("active"); // Удаляем класс, если вернулись к началу
+    }
+});
---
+++ js/validate.js
@@ -0,0 +1,65 @@
+import sendData from "./sendData.js";
+
+console.log(sendData);
+
+// Получаем ссылку на кнопку отправки
+const submitBtn = document.getElementById("submitBtn");
+
+// Получаем все поля
+const nameInput = document.querySelector(".form__name");
+const emailInput = document.querySelector(".form__email");
+const messageInput = document.querySelector(".form__message");
+
+// Функция для валидации имени
+function validateName() {
+    const namePattern = /^[A-Za-zА-Яа-яЁё\s]+$/; // Регулярное выражение для проверки только букв (включая кириллицу и пробелы)
+
+    if (nameInput.value.trim() === "" || !namePattern.test(nameInput.value)) {
+        nameInput.classList.add("false"); // Добавляем класс false
+        nameInput.classList.remove("true"); // Убираем класс true
+    } else {
+        nameInput.classList.add("true"); // Добавляем класс true
+        nameInput.classList.remove("false"); // Убираем класс false
+        return true;
+    }
+}
+
+// Функция для валидации email
+function validateEmail() {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!re.test(String(emailInput.value).toLowerCase())) {
+        emailInput.classList.add("false");
+        emailInput.classList.remove("true");
+    } else {
+        emailInput.classList.add("true");
+        emailInput.classList.remove("false");
+        return true;
+    }
+}
+
+// Функция для валидации сообщения
+function validateMessage() {
+    if (messageInput.value.trim() === "") {
+        messageInput.classList.add("false");
+        messageInput.classList.remove("true");
+    } else {
+        messageInput.classList.add("true");
+        messageInput.classList.remove("false");
+        return true;
+    }
+}
+
+// Добавляем обработчики на событие focusout (потеря фокуса)
+nameInput.addEventListener("focusout", validateName);
+emailInput.addEventListener("focusout", validateEmail);
+messageInput.addEventListener("focusout", validateMessage);
+
+// // Добавляем обработчик на кнопку отправки
+// submitBtn.addEventListener("click", function (event) {
+//     event.preventDefault(); // Предотвращаем отправку формы
+
+//     // Выполняем валидацию на нажатии кнопки
+//     if (validateName() && validateEmail() && validateMessage()) {
+//         sendData();
+//     }
+// });
